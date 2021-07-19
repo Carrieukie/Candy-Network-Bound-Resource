c1c51e023fe6127641986331a9490ca791a885ae
Carrieukie <kariuki931@gmail.com>
2021-07-19 07:32:24 +0300

    Added comments to the repository

--- app/src/main/java/com/karis/networkboundresource/repository/MainRepository.kt
+++ app/src/main/java/com/karis/networkboundresource/repository/MainRepository.kt
@@ -16,21 +16,27 @@ class MainRepository @Inject constructor(
     private val weatherDao = database.charactersDao()
 
     fun getCharacters() = networkBoundResource(
+        // pass in the logic to query data from the database
         query = {
             weatherDao.getCandy()
         },
+        // pass in the logic to fetch data from the api
         fetch = {
             delay(2000)
             apiService.getCandy()
         },
+
+        //pass in the logic to save the result to the local cache
         saveFetchResult = { candys ->
             database.withTransaction {
                 weatherDao.deleteAllCandy()
                 weatherDao.insertCandy(candys)
             }
         },
+
+        //pass in the logic to determine if the networking call should be made
         shouldFetch = {candys ->
-            candys.isNotEmpty()
+            candys.isEmpty()
         }
     )
 
